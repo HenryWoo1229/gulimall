200862b2853733f03cf652d6accef0cf3a82363d
wuhao <candyngwh@126.com>
2020-05-05 21:00:57 +0800

    抽象 common 模块，生成 product 的 CRUD

--- product/src/test/java/com/atguigu/gulimall/product/ProductApplicationTests.java
+++ product/src/test/java/com/atguigu/gulimall/product/ProductApplicationTests.java
@@ -1,13 +1,30 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import feign.QueryMap;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 @SpringBootTest
 class ProductApplicationTests {
 
+	@Autowired
+	BrandService brandService;
+
 	@Test
 	void contextLoads() {
+		BrandEntity brandEntity = new BrandEntity();
+		brandEntity.setName("iPhone");
+		brandService.save(brandEntity);
+
+		List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+		list.forEach(brandEntity1 -> System.out.println(brandEntity1.getBrandId() + ", " + brandEntity1.getName()));
 	}
 
 }
